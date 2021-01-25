4f33c17f609921c27a4355f08ad1351c845968f3
zhengbing <mydreambing@126.com>
2021-01-25 10:39:59 +0800

    initial cloud-penguins-workflow

---
+++ cloud-penguins-workflow/src/main/java/com/penguins/cloud/workflow/CloudPenguinsWorkflowApplication.java
@@ -0,0 +1,17 @@
+package com.penguins.cloud.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengbing
+ * @date 2021/1/25
+ * @email mydreambing@126.com
+ */
+@SpringBootApplication
+public class CloudPenguinsWorkflowApplication {
+
+  public static void main(String[] args) {
+      SpringApplication.run(CloudPenguinsWorkflowApplication.class,args);
+  }
+}
