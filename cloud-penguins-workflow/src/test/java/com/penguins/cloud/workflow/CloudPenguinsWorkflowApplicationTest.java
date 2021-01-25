94b6111871a2a63dee8438f21bac60281cad8df1
zhengbing <mydreambing@126.com>
2021-01-25 18:02:12 +0800

    initial cloud-penguins-workflow demo

---
+++ cloud-penguins-workflow/src/test/java/com/penguins/cloud/workflow/CloudPenguinsWorkflowApplicationTest.java
@@ -0,0 +1,9 @@
+package com.penguins.cloud.workflow;
+
+/**
+ * @author zhengbing
+ * @date 2021/1/25
+ * @email mydreambing@126.com
+ */
+public class CloudPenguinsWorkflowApplicationTest {
+}
---
+++ cloud-penguins-workflow/src/test/java/com/penguins/cloud/workflow/controller/DeploymentCotnrollerTest.java
@@ -0,0 +1,18 @@
+package com.penguins.cloud.workflow.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhengbing
+ * @date 2021/1/25
+ * @email mydreambing@126.com
+ */
+@SpringBootTest
+public class DeploymentCotnrollerTest {
+
+}
