ad5bedb3df9a9a0ce55294154bd0f665bb967066
zhengbing <mydreambing@126.com>
2021-03-20 13:10:14 +0800

    auth  base

--- cloud-penguins-auth/Auth.md
+++ cloud-penguins-auth/Auth.md
@@ -7,3 +7,23 @@ implementation 'org.springframework.boot:spring-boot-starter-web'
 implementation 'org.springframework.boot:spring-boot-starter-security'
 implementation 'org.springframework.cloud:spring-cloud-starter-bootstrap'
 ```
+
+## 启动应用
+
+进入应用
+<http://localhost:11011>
+
+会进入 spring security 默认的登陆页面
+![img.png](assert/login.png)
+
+默认用户名为：user 默认密码，在启动标准输出有打印出来
+![img.png](assert/password.png)
+
+在配置文件中配置默认用户名/密码
+
+```yaml
+  security:
+    user:
+      name: admin
+      password: admin
+```
--- cloud-penguins-auth/src/main/java/com/penguins/cloud/cloudpenguinsauth/CloudPenguinsAuthApplication.java
+++ cloud-penguins-auth/src/main/java/com/penguins/cloud/cloudpenguinsauth/CloudPenguinsAuthApplication.java
@@ -1,28 +1,21 @@
 package com.penguins.cloud.cloudpenguinsauth;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author zhengbing
  */
-@MapperScan({"com.penguins.cloud.cloudpenguinsauth.repository"})
+//@MapperScan({"com.penguins.cloud.cloudpenguinsauth.repository"})
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CloudPenguinsAuthApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CloudPenguinsAuthApplication.class, args);
   }
 
-  @Bean
-  public RestTemplate getRestTemplate() {
-    return new RestTemplate();
-  }
 }
