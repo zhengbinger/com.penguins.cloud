9bc05d05582ebff95ad5f08b4f869ead4e054450
zhengbing <mydreambing@126.com>
2021-04-24 01:09:18 +0800

    addition user module

--- cloud-penguins-auth/src/main/java/com/penguins/cloud/cloudpenguinsauth/CloudPenguinsAuthApplication.java
+++ cloud-penguins-auth/src/main/java/com/penguins/cloud/cloudpenguinsauth/CloudPenguinsAuthApplication.java
@@ -5,18 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @author zhengbing
- */
-@MapperScan({"com.penguins.cloud.cloudpenguinsauth.repository"})
+/** @author zhengbing */
+@MapperScan({"com.penguins.cloud.repository"})
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CloudPenguinsAuthApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CloudPenguinsAuthApplication.class, args);
   }
-
 }
---
+++ cloud-penguins-user/penguins-user-provider/src/main/java/com/penguins/cloud/PenguinsUserProviderApplication.java
@@ -0,0 +1,17 @@
+package com.penguins.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengbing
+ * @date 2021/4/24 00:44
+ * @email mydreambing@126.com
+ */
+@SpringBootApplication
+public class PenguinsUserProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PenguinsUserProviderApplication.class, args);
+  }
+}
