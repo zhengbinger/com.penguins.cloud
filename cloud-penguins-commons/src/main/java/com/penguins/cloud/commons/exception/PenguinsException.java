7c518adfbf7032f33b294596dbd8b4ad5b1f8fbd
zhengbing <mydreambing@126.com>
2021-05-07 09:25:55 +0800

    Global Exception

---
+++ cloud-penguins-commons/src/main/java/com/penguins/cloud/commons/exception/PenguinsException.java
@@ -0,0 +1,10 @@
+package com.penguins.cloud.commons.exception;
+
+/**
+ * @author zhengbing
+ * @date 2021/5/6 11:40
+ * @email mydreambing@126.com
+ */
+public class PenguinsException extends Exception {
+  private static final long serialVersionUID = -3286131001092080241L;
+}
---
+++ cloud-penguins-commons/src/main/java/com/penguins/cloud/commons/exception/PenguinsGlobalException.java
@@ -0,0 +1,36 @@
+package com.penguins.cloud.commons.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author zhengbing
+ * @date 2021/5/6 10:13
+ * @email mydreambing@126.com
+ */
+@ControllerAdvice
+public class PenguinsGlobalException extends ResponseEntityExceptionHandler {
+
+  /**
+   * 全局异常处理
+   *
+   * @param request http 请求
+   * @param e exception
+   * @return error
+   */
+  @ExceptionHandler(Exception.class)
+  ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable e) {
+    if (e instanceof PenguinsException) {
+      // 自定义异常
+    } else if (e instanceof NullPointerException) {
+      // 空指针异常
+    } else {
+      // 其他异常
+    }
+    return null;
+  }
+}
