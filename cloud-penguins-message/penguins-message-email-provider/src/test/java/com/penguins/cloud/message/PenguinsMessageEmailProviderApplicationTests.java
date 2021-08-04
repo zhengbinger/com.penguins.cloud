a383357691af23cf83eedb8171528ac940b719c9
zhengbing <mydreambing@126.com>
2021-08-04 17:18:20 +0800

    addition message module

---
+++ cloud-penguins-message/penguins-message-email-provider/src/test/java/com/penguins/cloud/message/PenguinsMessageEmailProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguins.cloud.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinsMessageEmailProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cloud-penguins-message/penguins-message-sms-provider/src/test/java/com/penguins/cloud/message/PenguinsMessageSmsProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penguins.cloud.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinsMessageSmsProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
