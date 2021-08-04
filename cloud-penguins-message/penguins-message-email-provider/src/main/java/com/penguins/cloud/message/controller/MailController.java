ab76d002037466a092e879d7b0b6540b20572acb
zhengbing <mydreambing@126.com>
2021-08-04 23:38:18 +0800

    rebuild  message module

--- cloud-penguins-message/penguins-message-email-provider/src/main/java/com/penguins/cloud/message/controller/MailController.java
+++ cloud-penguins-message/penguins-message-email-provider/src/main/java/com/penguins/cloud/message/controller/MailController.java
@@ -1,15 +1,12 @@
 package com.penguins.cloud.message.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 /**
  * @author zhengbing
  * @date 2021/5/7 10:51
  * @email mydreambing@126.com
  */
-@RestController
-@RequestMapping("mail")
+//@RestController
+//@RequestMapping("mail")
 public class MailController {
 
 //  @Autowired
