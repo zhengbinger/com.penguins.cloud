199eb17e58d8bbf4390554aec2b49680b335685a
zhengbing <mydreambing@126.com>
2021-08-05 10:38:01 +0800

    处理build问题

--- cloud-penguins-message/penguins-message-email-provider/src/main/java/com/penguins/cloud/message/PenguinsMessageEmailProviderApplication.java
+++ cloud-penguins-message/penguins-message-email-provider/src/main/java/com/penguins/cloud/message/PenguinsMessageEmailProviderApplication.java
@@ -3,6 +3,7 @@ package com.penguins.cloud.message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PenguinsMessageEmailProviderApplication {
 
