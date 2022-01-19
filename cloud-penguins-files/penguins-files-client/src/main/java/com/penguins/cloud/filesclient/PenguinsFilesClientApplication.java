60fdf8a667cbd890ab2bddce75e74d10ca71bb15
zhengbing <mydreambing@126.com>
2022-01-19 18:03:38 +0800

    update  format

---
+++ cloud-penguins-files/penguins-files-client/src/main/java/com/penguins/cloud/filesclient/PenguinsFilesClientApplication.java
@@ -0,0 +1,13 @@
+package com.penguins.cloud.filesclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinsFilesClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenguinsFilesClientApplication.class, args);
+    }
+
+}
