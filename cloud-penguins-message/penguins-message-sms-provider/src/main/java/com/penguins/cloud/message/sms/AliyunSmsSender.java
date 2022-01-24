134e5b5b3a6f4241ee00bfe68cbf64992e2c0dca
zhengbing <mydreambing@126.com>
2022-01-24 14:46:33 +0800

    file  module

--- cloud-penguins-message/penguins-message-sms-provider/src/main/java/com/penguins/cloud/message/sms/AliyunSmsSender.java
+++ cloud-penguins-message/penguins-message-sms-provider/src/main/java/com/penguins/cloud/message/sms/AliyunSmsSender.java
@@ -22,50 +22,50 @@ import java.util.Map;
 @Component
 public class AliyunSmsSender implements SmsSender {
 
-  private Logger log = LoggerFactory.getLogger(AliyunSmsSender.class);
-
-  /**
-   * 使用AK&SK初始化账号Client
-   *
-   * @param accessKeyId String
-   * @param accessKeySecret String
-   * @return Client
-   */
-  public static Client createClient(String accessKeyId, String accessKeySecret) {
-    Config config =
-        new Config()
-            // 您的AccessKey ID
-            .setAccessKeyId(accessKeyId)
-            // 您的AccessKey Secret
-            .setAccessKeySecret(accessKeySecret);
-    // 访问的域名
-    config.endpoint = SmsConstants.ALIYUN_ENDPOINT;
-    try {
-      return new Client(config);
-    } catch (Exception e) {
-      e.printStackTrace();
+    private Logger log = LoggerFactory.getLogger(AliyunSmsSender.class);
+  
+    /**
+     * 使用AK&SK初始化账号Client
+     *
+     * @param accessKeyId     String
+     * @param accessKeySecret String
+     * @return Client
+     */
+    public static Client createClient(String accessKeyId, String accessKeySecret) {
+        Config config =
+                new Config()
+                        // 您的AccessKey ID
+                        .setAccessKeyId(accessKeyId)
+                        // 您的AccessKey Secret
+                        .setAccessKeySecret(accessKeySecret);
+        // 访问的域名
+        config.endpoint = SmsConstants.ALIYUN_ENDPOINT;
+        try {
+            return new Client(config);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-    return null;
-  }
 
-  @Autowired
-  private AliyunConfigureProperties aliyunConfigureProperties;
+    @Autowired
+    private AliyunConfigureProperties aliyunConfigureProperties;
 
-  @Override
-  @ConditionalOnBean(AliyunConfigureProperties.class)
-  public void send(Map<String, Object> params) throws Exception {
-    log.info("发送短信到XXX");
-    Client client =
-        AliyunSmsSender.createClient(aliyunConfigureProperties.getAccessKeyId(),aliyunConfigureProperties.getAccessKeySecret());
-    SendSmsRequest sendSmsRequest = new SendSmsRequest();
-    sendSmsRequest.setPhoneNumbers("15918713381");
-    sendSmsRequest.setSignName("郑冰");
-    sendSmsRequest.setTemplateCode("SMS_42685022");
-    sendSmsRequest.setTemplateParam("{\"name\":\"test\",\"day\":5}");
-    // 复制代码运行请自行打印 API 的返回值
-    assert client != null;
-    SendSmsResponse smsResponse = client.sendSms(sendSmsRequest);
-    System.out.println(smsResponse.getBody().message);
-  }
+    @Override
+    @ConditionalOnBean(AliyunConfigureProperties.class)
+    public void send(Map<String, Object> params) throws Exception {
+        log.info("发送短信到XXX");
+        Client client =
+                AliyunSmsSender.createClient(aliyunConfigureProperties.getAccessKeyId(), aliyunConfigureProperties.getAccessKeySecret());
+        SendSmsRequest sendSmsRequest = new SendSmsRequest();
+        sendSmsRequest.setPhoneNumbers("15918713381");
+        sendSmsRequest.setSignName("郑冰");
+        sendSmsRequest.setTemplateCode("SMS_42685022");
+        sendSmsRequest.setTemplateParam("{\"name\":\"test\",\"day\":5}");
+        // 复制代码运行请自行打印 API 的返回值
+        assert client != null;
+        SendSmsResponse smsResponse = client.sendSms(sendSmsRequest);
+        System.out.println(smsResponse.getBody().message);
+    }
 
 }
